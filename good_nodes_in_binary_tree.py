7ed4ccf12d08c83cc475b6547cc24da23c9b98a0
Vaclav Kosar <admin@vaclavkosar.com>
2022-06-12 06:53:39 +0200

    solution

---
+++ good_nodes_in_binary_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        # I can use backtracking with depth first search
+        # can recursively travel the tree and check the condition by keeping max value
+        # actually I must crawl all nodes as there is no sort
+        if root is None:
+            return 0
+
+        else:
+            return 1 + self.n_good_nodes(root.left, root.val) + self.n_good_nodes(root.right, root.val)
+
+    def n_good_nodes(self, node, max_val) -> int:
+        if node is None:
+            return 0
+
+        good_count = 0
+        if node.val >= max_val:
+            good_count += 1
+
+        max_val = max(node.val, max_val)
+        good_count += self.n_good_nodes(node.left, max_val)
+        good_count += self.n_good_nodes(node.right, max_val)
+        return good_count
+
+

5b865302f61c3dc50903519859a3b80071e8452f
Vaclav Kosar <admin@vaclavkosar.com>
2022-07-10 12:42:50 +0200

    solution

--- append_k_ints_min_sum.py
+++ append_k_ints_min_sum.py
@@ -11,10 +11,11 @@ class Solution:
         Solution will be iterating over non existing ints in the nums from the smallest.
         Easiest is probably to sort a then iterate with skipping.
         This implies N Log N complexity.
+        
+        
+        # Iterating over K is very slow as K can be very large in this problem.
+        # We don't need to do this, since the sums can be calculated fast.
 
-        """
-
-        nums.sort()
         i = 0
         count = 0
         k_attempt = 1
@@ -48,6 +49,75 @@ class Solution:
         raise ValueError()
 
 
+        # Instead we can binary search for the K using the summation formula for K.
+        # Thanks to boundaries the search is Log N.
+        # This is still too complicated. Notice that we are always thinking only about the nums.
+
+        nums = list(set(nums))
+        nums.sort()
+        k_right = k + len(nums)
+        k_left = k
+
+        while True:
+            k_max = (k_left + k_right) // 2
+            n_arr_based_on_k = [n for n in nums if n <= k_max]
+            if k_max - len(n_arr_based_on_k) < k:
+                k_left = k_max + 1
+
+            elif k_max - len(n_arr_based_on_k) > k:
+                k_right = k_max - 1
+
+            if k_max - len(n_arr_based_on_k) == k:
+                return k_max * (k_max + 1) // 2 - sum(n_arr_based_on_k)
+
+        # So instead, just find the first of the nums `i` that we will not be skipping during the summation.
+        """
+
+        nums = list(set(nums))
+        nums.sort()
+
+        if nums[len(nums) - 1] <= k + len(nums):
+            j = k + len(nums)
+            res = (j + 1) * j // 2 - sum(nums)
+            return res
+
+        i_left = 0
+        i_right = len(nums) - 1
+        while i_left < i_right:
+            i = (i_left + i_right) // 2
+            k_guess = nums[i] - (i + 1)
+
+            if k_guess <= k:
+                i_left = i + 1
+
+            else:
+                i_right = i
+
+        j = i_left + k
+        res = (j + 1) * j // 2 - sum(nums[:i_left])
+        return res
+
+
+assert Solution().minimalKSum(
+    [1, 4, 25, 10, 25]
+    , 2
+) == 5
+
+
+assert Solution().minimalKSum(
+[5,6],
+6
+) == 25
+
 assert Solution().minimalKSum(
     [96, 44, 99, 25, 61, 84, 88, 18, 19, 33, 60, 86, 52, 19, 32, 47, 35, 50, 94, 17, 29, 98, 22, 21, 72, 100, 40, 84],
-    35) == 794
+    35
+) == 794
+
+assert Solution().minimalKSum(
+    [20, 83, 66, 95, 76, 36, 44, 40, 21, 93, 83, 51, 8, 26, 96, 42, 4, 64, 63, 72, 18, 80, 25, 27, 22, 68, 27, 24, 83,
+     57, 4, 54, 35, 38, 52, 63, 25, 66, 32, 75, 22, 97, 54, 83, 40, 46, 9, 84, 76, 46, 56, 44, 75, 21, 4, 28, 67, 79,
+     48, 52, 83, 82, 73, 99, 75, 10, 51, 92, 49, 79, 58, 54, 33, 19, 96, 11, 27, 48, 42, 6, 27, 96, 7, 26, 26, 50, 97,
+     83, 14],
+    24
+) == 710

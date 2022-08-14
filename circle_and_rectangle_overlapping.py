a52d48e0c1745f0aa82d1104d57c848f0913a8a4
Vaclav Kosar <admin@vaclavkosar.com>
2022-08-14 09:08:48 +0200

    solution

--- circle_and_rectangle_overlapping.py
+++ circle_and_rectangle_overlapping.py
@@ -1,6 +1,3 @@
-import math
-
-
 class Solution:
 
     def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
@@ -11,10 +8,12 @@ class Solution:
         def get_closest(x1, x2, x):
             return min(max(x1, x), x2)
 
-        # find the closest points
+        # Find the closest point of the rectangle.
+        # It lies on the rectangle border, and because of circle symmetry it is the point closest to the circle center.
         x = get_closest(x1, x2, xCenter)
         y = get_closest(y1, y2, yCenter)
 
+        # Is the point within the radius?
         return radius ** 2 >= (x - xCenter) ** 2 + (y - yCenter) ** 2
 
 

893fb96f1d648b4e1eb37da2580e0f73755b5c38
Vaclav Kosar <admin@vaclavkosar.com>
2023-10-06 06:17:01 +0200

    solution

--- calculate_money_in_leetcode_bank.py
+++ calculate_money_in_leetcode_bank.py
@@ -1,4 +1,5 @@
-# TODO finish
+# From Simplest to The Fastest with Unit Tests For Quick Understanding
+# Follow Vaclav Kosar for more software and machine learning at https://vaclavkosar.com/
 
 class Solution:
     def totalMoney(self, n: int) -> int:
@@ -33,41 +34,7 @@ class Solution:
         The formula must involve the 7-reminder, week savings calculation,
             and modification for the growth.
 
-        If we are within the week the savings of the week are:
-        3 days is odd: (3 + 1) + (3 + 1) / 2 = 3 / 2 * (3+1)
-        4 days is even: 2 * (4 + 1)  = 4 / 2 * (4+1)
-        i days: ( i / 2 * (i + 1))
-
-        Week 0 Sunday = 0
-        Week 1 Sunday = 28
-        Week 2 Sunday = 14 + 28 = 42
-        Sum =
-
-        The week starts with n_week, progresses to n_week + 7.
-        Week 1: 28
-        Week 2: 2+3+4+5+6+7+8 = 7 * 1 + 1+2+3+4+5+6+7
-        So each week it is `7 * n_week + 28`.
-
-        10 = Week 1 + Wednesday: 28 + 2 + 3 + 4 = 28 + (3 * 1) + 1 + 2 + 3 = 37
-
-
-        # The analytical formula did not work out yet.
-
-        reminder_days = n % 7
-        n_full_weeks = n // 7
-
-        full_weeks_sum = sum(i * 7 + 28 for i in range(n_full_weeks))
-        assert full_weeks_sum == int((28 + (n_full_weeks - 1) * 7 + 28) * n_full_weeks / 2)
-        assert n_full_weeks * reminder_days + int(reminder_days / 2 * (reminder_days + 1)) == sum(i * (n_full_weeks + 1) for i in range(1, reminder_days + 1))
-
-        if reminder_days == 0:
-            return full_weeks_sum * n_full_weeks
-
-        else:
-            return full_weeks_sum + n_full_weeks * reminder_days + int(reminder_days / 2 * (reminder_days + 1))
-
-        """
-
+        ```
         save_amount = 1
         saving_last_monday = save_amount
         total = 0
@@ -81,3 +48,45 @@ class Solution:
             save_amount += 1
 
         return total
+        ```
+
+        # Reminder days calculation
+
+        If we are within the week the savings of the week are:
+        3 days is odd: (3 + 1) + (3 + 1) / 2 = 3 / 2 * (3+1)
+        4 days is even: 2 * (4 + 1)  = 4 / 2 * (4+1)
+        i days: ( i / 2 * (i + 1))
+
+        Let's verify that the formula works for the reminder days.
+        `assert n_full_weeks * reminder_days + int(reminder_days / 2 * (reminder_days + 1)) == sum(i + n_full_weeks for i in range(1, reminder_days + 1))`
+
+
+        # Full Weeks Formula
+
+        Week 0 Sunday = 0
+        Week 1 Sunday = 28
+        Week 2 Sunday = 14 + 28 = 42
+        Sum = ...
+
+        The week starts with n_week, progresses to n_week + 7.
+        Week 1: 28
+        Week 2: 2+3+4+5+6+7+8 = 7 * 1 + 1+2+3+4+5+6+7
+        So each week it is `7 * n_week + 28`.
+
+        10 = Week 1 + Wednesday: 28 + 2 + 3 + 4 = 28 + (3 * 1) + 1 + 2 + 3 = 37
+
+        Let's verify that the formula works for the full weeks.
+        `assert (28 + (n_full_weeks - 1) * 7 + 28) * n_full_weeks // 2 == sum(i * 7 + 28 for i in range(n_full_weeks))`
+
+        """
+
+        reminder_days = n % 7
+        n_full_weeks = n // 7
+
+        full_weeks_sum = sum(i * 7 + 28 for i in range(n_full_weeks))
+
+        if reminder_days == 0:
+            return full_weeks_sum
+
+        else:
+            return full_weeks_sum + n_full_weeks * reminder_days + (reminder_days * (reminder_days + 1)) // 2

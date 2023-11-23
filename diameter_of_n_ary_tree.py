7fe2e03999e2824248f2e78625bf8d892c627c16
Vaclav Kosar <admin@vaclavkosar.com>
2023-11-23 20:52:07 +0100

    solution

--- diameter_of_n_ary_tree.py
+++ diameter_of_n_ary_tree.py
@@ -19,48 +19,64 @@ class Solution:
 
         # Longest shortest path of 3 here is between (2, 5) or (4, 5).
         >>> Solution().diameter(NAryTreeNode.from_list([1, None, 3, 2, 4, None, 5, 6]))
-        4
+        3
 
         >>> Solution().diameter(NAryTreeNode.from_list([1, None, 2, None, 3, 4, None, 5, None, 6]))
-        6
+        4
 
         >>> Solution().diameter(NAryTreeNode.from_list([1]))
         0
 
+        >>> Solution().diameter(NAryTreeNode.from_list([3, None, 1, None, 5]))
+        2
+
+
         # What is irrelevant, risk, another unknown?
 
         Do not only focus on the shortest path between any two nodes in the tree.
-        The actual values do not matter.
+        The node values do not matter.
 
 
         # Comparison of Extreme Approaches To Find The Best Solution
 
         - Find the shortest path between all nodes. Complexity O(N**2).
-        - Since the path can intersect itself, it can always go through the root, which is the longest option. Thanks to this we need to only find 2 longest paths from the root. To break the problem down, traverse the tree and do the same for each node recursively. Once we have the values of lengths for root we are done. A leaf does not have two paths, so they can be returned both as zero.
-        - TODO Alternative: Avoid intersecting the path: Return the longest path in the subtree, and longest branch in the subtree.
+        - The paths cannot intersect itself. It can go through the root.
+        - To break the problem down, traverse the tree and do the same for each node recursively.
+          Avoid intersecting the path.
+          Return the longest path in the subtree, and longest branch in the subtree.
+          Once we have the values of lengths for root we are done.
+          A leaf does not have two paths, so they can be returned both as zero.
+
 
         # Implementation
         """
 
         def get_two_longest(node: NAryTreeNode):
             if node.children is None or len(node.children) == 0:
-                return [0]
+                return 0, 0
 
-            longest = []
-            # itertools.chain(two_longest(child) for child in node.children)
+            longest_sub_branches = []
+            longest_sub_paths = []
             for child in node.children:
-                longest.extend(get_two_longest(child))
+                longest_sub_branch, longest_sub_path = get_two_longest(child)
+                longest_sub_branches.append(longest_sub_branch)
+                longest_sub_paths.append(longest_sub_path)
 
-            longest = heapq.nlargest(2, longest)
-            # we made a step up
-            longest = [i + 1 for i in longest]
-            return longest
+            longest_sub_branches = heapq.nlargest(2, longest_sub_branches)
+            longest_sub_path = max(longest_sub_paths)
+            longest_sub_branches = [i + 1 for i in longest_sub_branches]
 
-        longest = get_two_longest(root)
-        if len(longest) == 1:
-            return 0
+            if len(longest_sub_branches) == 1:
+                longest_path = max(longest_sub_path, longest_sub_branches[0])
 
-        return longest[0] + longest[1]
+            else:
+                longest_path = max(longest_sub_branches[0] + longest_sub_branches[1], longest_sub_branches[0], longest_sub_path)
+
+            return longest_sub_branches[0], longest_path
+
+        longest_sub_branch, longest_path = get_two_longest(root)
+
+        return longest_path
 
 
 

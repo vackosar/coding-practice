5eea2b9b416d8f0eec2999ff6e6a60a452483174
Vaclav Kosar <admin@vaclavkosar.com>
2024-04-06 05:13:51 +0200

    solution

---
+++ convert_the_temperature.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        """
+        The input is celsius temperature which is rounded to 2 decimal places.
+        Convert to Kelvin and Fahrenehit.
+        Return [kelvin, fahrenheit]
+
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.8 + 32
+
+        # Examples
+        >>> Solution().convertTemperature(36.5)
+        [309.65, 97.7]
+
+        >>> Solution().convertTemperature(122.11)
+        [395.26, 251.798]
+
+        """
+
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.8 + 32
+
+        return [kelvin, fahrenheit]
